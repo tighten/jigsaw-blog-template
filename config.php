9236f48469ce24227415870436f630825a17feaf
Matt Stauffer <matt@tighten.co>
2018-05-16 21:28:29 -0400

    Fix blog author

--- config.php
+++ config.php
@@ -2,12 +2,13 @@
 
 return [
     'blogTitle' => 'My Blog About Jigsaws',
-    'author' => 'Matt Stauffer',
+    'blogAuthor' => 'Matt Stauffer',
     'baseUrl' => 'http://jigsaw-blog-skeleton.test/',
     'searchDomain' => 'jigsaw-blog-skeleton.text', // for Google search
     'production' => false,
     'collections' => [
         'posts' => [
+            'author' => 'Matt Stauffer', // Default author if not provided in given post
             'sort' => '-date',
             'path' => 'blog/{date|Y-m-d}/{filename}',
             'excerpt' => function ($page, $characters = 50) {
