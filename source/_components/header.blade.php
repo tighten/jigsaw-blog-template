478571c044e460b154cff605179c6005bab8358f
Matt Stauffer <matt@tighten.co>
2018-05-16 21:40:42 -0400

    Tweak nav responsivity

--- source/_components/header.blade.php
+++ source/_components/header.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-grey-lighter mb-8">
     <header class="container mx-auto px-4">
         <div class="sm:flex">
-            <a href="/" class="inline-flex items-center w-full py-6 w-full font-bold">
+            <a href="/" class="inline-flex items-center w-full pt-4 pb-2 sm:py-6 w-full font-bold">
                 @include('_components.logo')
                 <span class="pl-4">
                     {{ $page->blogTitle }}
@@ -9,20 +9,20 @@
             </a>
 
             <nav class="sm:flex sm:flex-wrap sm:justify-end lg:w-full">
-                <a href="/" class="flex items-center p-4">
+                <a href="/" class="flex items-center p-2 sm:p-4">
                     Blog
                 </a>
-                <a href="/about" class="flex items-center p-4">
+                <a href="/about" class="flex items-center p-2 sm:p-4">
                     About
                 </a>
-                <a href="/contact" class="flex items-center p-4">
+                <a href="/contact" class="flex items-center p-2 sm:p-4">
                     Contact
                 </a>
 
                 <form action="http://google.com/search" method="get" class="flex items-center px-2">
                     <fieldset role="search">
                         <input type="hidden" name="sitesearch" value="{{ $page->searchDomain }}">
-                        <input type="text" name="q" results="0" placeholder="Search" class="p-1 border border-grey-dark">
+                        <input type="text" name="q" results="0" placeholder="Search" class="p-1 border border-grey-dark my-4 sm:mt-0 lg:mb-0">
                     </fieldset>
                 </form>
             </nav>
