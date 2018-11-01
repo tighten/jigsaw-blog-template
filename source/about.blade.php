4ac6c977bc8993326b4b7e6a94d092af70af8e62
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-01 16:18:58 -0400

    Update about page markup
    - responsive styles for avatar
    - add heading styles to `_base.scss`

--- source/about.blade.php
+++ source/about.blade.php
@@ -11,10 +11,10 @@
 <div class="w-full max-w-xl mx-auto px-6 transition-sm md:px-0">
     <h1 class="font-bold text-black">About</h1>
 
-    <p class="mb-6">
-        <img src="https://images.unsplash.com/photo-1532560460156-99a8b937ee8c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&h=300&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=8490222c3ca7a048fcdbfed66fe454b5" class="rounded-full w-50 float-right my-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo natus, amet ! Quod  laudantium, aperiam in omnis similique! Voluptate sit suscipit aperiam distinctio laudantium maiores eveniet in, inventore recusandae.
-    </p>
+    <img src="https://images.unsplash.com/photo-1532560460156-99a8b937ee8c?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&h=300&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=8490222c3ca7a048fcdbfed66fe454b5"
+        class="flex rounded-full w-50 mx-auto my-10 md:float-right md:ml-10">
+
+    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo natus, amet ! Quod  laudantium, aperiam in omnis similique! Voluptate sit suscipit aperiam distinctio laudantium maiores eveniet in, inventore recusandae.</p>
     <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas.  fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas.  fugit natus deserunt atque veniam!</p>
 </div>
