c2c72161abfc4b363aed754a6e61ade3d31c5357
Keith Damiani <keith@tighten.co>
2018-11-26 16:37:19 -0500

    Update source/404.blade.php
    Co-Authored-By: anthonyterrell <anthonygterrell@gmail.com>

--- source/404.blade.php
+++ source/404.blade.php
@@ -8,7 +8,7 @@
         <hr class="block w-full max-w-sm mx-auto border my-8">
 
         <p class="text-xl">
-            Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/"> Jigsaw documentation</a>.
+            Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/">Jigsaw documentation</a>.
         </p>
     </div>
 @endsection
