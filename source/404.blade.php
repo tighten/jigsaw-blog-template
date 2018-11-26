fad3b28b4d7a3e03e1ec2d73cf1a38e98c104207
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-26 16:22:40 -0500

    Reorder tailwind classes

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,11 +1,11 @@
 @extends('_layouts.master')
 
 @section('body')
-    <div class="flex flex-col items-center mt-32 text-grey-darker">
+    <div class="flex flex-col items-center text-grey-darker mt-32">
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
         <h2 class="text-3xl">Page not found.</h2>
 
-        <hr class="block w-full max-w-sm mx-auto my-8 border">
+        <hr class="block w-full max-w-sm mx-auto border my-8">
 
         <p class="text-xl">
             Need to update this page? See the <a title="404 Page Documentation" href="https://jigsaw.tighten.co/docs/custom-404-page/"> Jigsaw documentation</a>.
