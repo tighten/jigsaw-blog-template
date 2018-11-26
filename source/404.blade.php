4881b3b3db67ec14319bce0281ddc183486b6718
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-26 16:48:05 -0500

    Add linebreak before h2

--- source/404.blade.php
+++ source/404.blade.php
@@ -3,6 +3,7 @@
 @section('body')
     <div class="flex flex-col items-center text-grey-darker mt-32">
         <h1 class="text-6xl font-light leading-none mb-2">404</h1>
+
         <h2 class="text-3xl">Page not found.</h2>
 
         <hr class="block w-full max-w-sm mx-auto border my-8">
