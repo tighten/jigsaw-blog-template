7edb8cf7d95494e200f72734d374189c962eb2af
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-26 09:39:47 -0500

    Update markup and styles in Search.vue

--- source/_assets/js/components/Search.vue
+++ source/_assets/js/components/Search.vue
@@ -8,18 +8,18 @@
 			autocomplete="off"
 			class="w-3/4 px-6 py-4 bg-grey-lighter focus:bg-grey-lightest border focus:border-blue-light outline-none cursor-pointer transition-fast">
 
-		<div v-if="results" class="absolute w-3/4 pt-4 mt-8 pt-1 flex flex-col text-grey text-sm">
+		<div v-if="results" class="absolute flex flex-col w-3/4 pt-4 mt-8 text-grey text-sm">
 			<div :key="result.link"
 				v-for="result in results"
-				class="px-6 py-3 bg-white border border-blue-light border-t-0 cursor-pointer hover:bg-grey-lighter transition-fast shadow">
+				class="bg-white hover:bg-grey-lighter px-6 py-3 border border-blue-light border-t-0 cursor-pointer shadow transition-fast">
 				<a :href="result.link" :title="result.title" class="text-xl">{{ result.title }}</a>
 
 				<p class="my-1">{{ result.snippet }}</p>
 			</div>
 
 			<div v-if="(! results.length) && query != ''"
-				class="px-6 py-3 bg-white border border-blue-light border-t-0 cursor-pointer hover:bg-grey-lighter transition-fast shadow">
-				<p class="">We didn't find anything for <span class="p-2 bg-grey-lightest font-bold">{{ query }}</span>. Please try searching again.</p>
+				class="bg-white hover:bg-grey-lighter px-6 py-3 border border-blue-light border-t-0 cursor-pointer shadow transition-fast">
+				<p>We didn't find anything for <strong class="bg-grey-lightest p-2">{{ query }}</strong>. Please try searching again.</p>
 			</div>
 		</div>
 	</div>
