238bf4f00e5d7e68738842e7b1d938c83105d3c5
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-27 13:31:13 -0500

    Update contact markup

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -62,7 +62,7 @@
             <p>
                 &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
 
-                <br class="lg:hidden">
+                <br class="md:hidden">
 
                 Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
                 and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
