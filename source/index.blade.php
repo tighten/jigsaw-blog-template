5f7146bbdca418178751cd9d3e28506ac0bb3309
Anthony Terrell <anthonygterrell@gmail.com>
2018-11-28 16:00:58 -0500

    Update homepage listing
    - query by category
    - order posts
    - remove category from getting started.

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,40 +8,40 @@
 @endpush
 
 @section('body')
-@if($featuredPost = $posts->where('featured')->first())
-    <div class="w-full pb-8 mb-4 border-b">
-        @if($featuredPost->cover_image)
-            <img src="{{ $featuredPost->cover_image }}" alt="{{ $featuredPost->title }} cover image">
-        @endif
+    @if ($featuredPost = $posts->where('featured')->first())
+        <div class="w-full pb-8 mb-4 border-b">
+            @if($featuredPost->cover_image)
+                <img src="{{ $featuredPost->cover_image }}" alt="{{ $featuredPost->title }} cover image">
+            @endif
 
-        <h2 class="text-3xl">
-            <a href="{{ $page->url($featuredPost->getPath()) }}" title="Read {{ $featuredPost->title }}" class="text-black font-extrabold">
-                {{ $featuredPost->title }}
-            </a>
-        </h2>
+            <h2 class="text-3xl">
+                <a href="{{ $page->url($featuredPost->getPath()) }}" title="Read {{ $featuredPost->title }}" class="text-black font-extrabold">
+                    {{ $featuredPost->title }}
+                </a>
+            </h2>
 
-        <p class="mt-0 mb-4 font-light">{!! $featuredPost->excerpt() !!}</p>
+            <p class="mt-0 mb-4 font-light">{!! $featuredPost->excerpt() !!}</p>
 
-        <a href="{{ $page->url($featuredPost->getPath()) }}" title="Read - {{ $featuredPost->title }}"
-            class="mb-4 text-blue uppercase font-semibold tracking-wide">Read</a>
+            <a href="{{ $page->url($featuredPost->getPath()) }}" title="Read - {{ $featuredPost->title }}"
+                class="mb-4 text-blue uppercase font-semibold tracking-wide">Read</a>
+        </div>
+    @endif
+
+    <div class="flex flex-col md:flex-row md:-mx-6">
+        @foreach ($posts->where('categories', ['configuration'])->take(2) as $post)
+            <div class="w-full md:w-1/2 md:mx-6">
+                @include('_components.post-preview-inline')
+            </div>
+        @endforeach
     </div>
-@endif
 
-<div class="flex flex-col md:flex-row md:-mx-6">
-    @foreach($posts->take(2) as $post)
-        <div class="w-full md:w-1/2 md:mx-6">
-            @include('_components.post-preview-inline')
-        </div>
-    @endforeach
-</div>
+    @include('_components.newsletter-signup')
 
-@include('_components.newsletter-signup')
-
-<div class="flex flex-col md:flex-row md:-mx-6">
-    @foreach($posts->take(2) as $post)
-        <div class="w-full md:w-1/2 md:mx-6">
-            @include('_components.post-preview-inline')
-        </div>
-    @endforeach
-</div>
+    <div class="flex flex-col md:flex-row md:-mx-6">
+        @foreach ($posts->where('categories', ['feature'])->take(2) as $post)
+            <div class="w-full md:w-1/2 md:mx-6">
+                @include('_components.post-preview-inline')
+            </div>
+        @endforeach
+    </div>
 @stop
