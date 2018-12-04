bbd758f1bbe22d5c04c99f9b05f9ab3439c5a038
Keith Damiani <keith@tighten.co>
2018-12-04 11:30:46 -0500

    Update About image

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,10 +10,11 @@
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/about.png"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-10 md:ml-10">
+    <img src="/assets/img/about.png" class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo natus, amet ! Quod  laudantium, aperiam in omnis similique! Voluptate sit suscipit aperiam distinctio laudantium maiores eveniet in, inventore recusandae.</p>
+
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas.  fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas.  fugit natus deserunt atque veniam!</p>
 @endsection
