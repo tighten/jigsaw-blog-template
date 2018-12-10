b9dc7d62609ca07578eeab15f655725ef8f838ab
Anthony Terrell <anthonygterrell@gmail.com>
2018-12-10 15:18:34 -0500

    Sort tailwindcss classes

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -28,13 +28,13 @@
 
     <body class="bg-grey-lightest text-blue-darkest leading-normal text-lg font-sans">
         <div id="vue-app">
-            <header class="bg-white border-b flex shadow-lg py-4" role="banner">
-                <div class="container max-w-4xl flex mx-auto px-4 lg:px-8">
+            <header class="flex bg-white border-b shadow-lg py-4" role="banner">
+                <div class="flex container max-w-4xl mx-auto px-4 lg:px-8">
                     <div class="flex items-center">
                         <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center font-bold">
-                            <img class="h-8 mr-3 md:h-10" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
+                            <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
 
-                            <h1 class="hidden text-2xl my-0 text-blue-darker md:block">{{ $page->siteName }}</h1>
+                            <h1 class="hidden md:block text-2xl text-blue-darker my-0">{{ $page->siteName }}</h1>
                         </a>
                     </div>
 
