6449a9880ff55780d3dea5b4155cfbf1f3316c63
Keith Damiani <keith@tighten.co>
2018-12-18 17:35:07 -0500

    Use `getUrl` in pagination links

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -19,7 +19,7 @@
     @if ($page->categories)
         @foreach ($page->categories as $i => $category)
             <a
-                href="{{ $page->url('/blog/categories/' . $category) }}"
+                href="{{ '/blog/categories/' . $category }}"
                 title="View posts in {{ $category }}"
                 class="inline-block bg-grey-light hover:bg-blue-lighter leading-loose tracking-wide text-grey-darkest uppercase text-xs font-semibold rounded mr-4 px-3 pt-px"
             >{{ $category }}</a>
@@ -33,7 +33,7 @@
     <nav class="flex justify-between text-sm md:text-base">
         <div>
             @if ($next = $page->getNext())
-                <a href="{{ $page->url($next->getPath()) }}" title="Older Post: {{ $next->title }}">
+                <a href="{{ $next->getUrl() }}" title="Older Post: {{ $next->title }}">
                     &LeftArrow; {{ $next->title }}
                 </a>
             @endif
@@ -41,7 +41,7 @@
 
         <div>
             @if ($previous = $page->getPrevious())
-                <a href="{{ $page->url($previous->getPath()) }}" title="Newer Post: {{ $previous->title }}">
+                <a href="{{ $previous->getUrl() }}" title="Newer Post: {{ $previous->title }}">
                     {{ $previous->title }} &RightArrow;
                 </a>
             @endif
