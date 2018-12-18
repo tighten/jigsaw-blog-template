1ec13207b9b13e70ec94c988510cb889dd8e0dfa
Keith Damiani <keith@tighten.co>
2018-12-17 22:09:04 -0500

    Update GenerateSitemap to match docs version, warn if no baseUrl

--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -1,26 +1,42 @@
-<?php namespace App\Listeners;
+<?php
 
-use TightenCo\Jigsaw\Jigsaw;
+namespace App\Listeners;
+
 use samdark\sitemap\Sitemap;
+use TightenCo\Jigsaw\Jigsaw;
 
 class GenerateSitemap
 {
+    protected $exclude = [
+        '/assets/*',
+        '*/favicon.ico',
+        '*/404'
+    ];
+
     public function handle(Jigsaw $jigsaw)
     {
         $baseUrl = $jigsaw->getConfig('baseUrl');
+
+        if (! $baseUrl) {
+            echo("To generate a sitemap.xml file, please specify a 'baseUrl' in config.php.\n");
+
+            return;
+        }
+
         $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
 
-        collect($jigsaw->getOutputPaths())->each(function ($path) use ($baseUrl, $sitemap) {
-            if (! $this->isAsset($path)) {
+        collect($jigsaw->getOutputPaths())
+            ->reject(function ($path) {
+                return $this->isExcluded($path);
+            })->each(function ($path) use ($baseUrl, $sitemap) {
                 $sitemap->addItem(rtrim($baseUrl, '/') . $path, time(), Sitemap::DAILY);
-            }
         });
 
         $sitemap->write();
     }
 
-    public function isAsset($path)
+    public function isExcluded($path)
     {
-        return starts_with($path, '/assets');
+        return str_is($this->exclude, $path);
     }
 }
