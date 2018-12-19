ba8d90b883de58cdb52ca3baeb4b15b8a2eca307
Keith Damiani <keith@tighten.co>
2018-12-18 20:48:32 -0500

    Add PurgeCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build/');
@@ -24,5 +25,10 @@ mix.js('source/_assets/js/main.js', 'js')
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
+    })
+    .purgeCss({
+        extensions: ['html', 'md', 'js', 'php', 'vue'],
+        folders: ['source'],
+        whitelistPatterns: [/language/, /hljs/],
     })
     .version();
