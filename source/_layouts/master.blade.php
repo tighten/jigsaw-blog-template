a86b6e47d371474804abd1b76a0e62df754e126e
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2019-08-14 23:35:19 -0700

    css cleanup, list-reset removed from Tailwind 1.x

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -55,7 +55,7 @@
         </main>
 
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
-            <ul class="flex flex-col md:flex-row justify-center list-reset">
+            <ul class="flex flex-col md:flex-row justify-center">
                 <li class="md:mr-2">
                     &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
                 </li>
