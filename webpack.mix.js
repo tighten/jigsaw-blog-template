a1830db195ba4b5392a21b22ac73c96240cd4150
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-04-17 14:58:29 -0400

    Add laravel-mix-jigsaw and remove unneeded build tasks and dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,34 +1,25 @@
-let mix = require('laravel-mix');
-let build = require('./tasks/build.js');
-let tailwindcss = require('tailwindcss');
+const mix = require('laravel-mix');
+require('laravel-mix-jigsaw');
 require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/build/');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch([
-            'config.php',
-            'source/**/*.md',
-            'source/**/*.php',
-            'source/**/*.scss',
-        ]),
-    ],
-});
+mix.setPublicPath('source/assets/build');
 
 mix.js('source/_assets/js/main.js', 'js')
-    .sourceMaps()
     .sass('source/_assets/sass/main.scss', 'css/main.css')
-    .sourceMaps()
+    .jigsaw({
+        watch: ['config.php', 'source/**/*.md', 'source/**/*.php', 'source/**/*.scss'],
+    })
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss()],
+        postCss: [
+            require('tailwindcss'),
+        ],
     })
     .purgeCss({
         extensions: ['html', 'md', 'js', 'php', 'vue'],
         folders: ['source'],
         whitelistPatterns: [/language/, /hljs/, /mce/],
     })
+    .sourceMaps()
     .version();
