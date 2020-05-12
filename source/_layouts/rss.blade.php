db8e29802658c7afc0ced617dc3bcea29da85865
Keith Damiani <keith@tighten.co>
2020-05-12 09:45:49 -0400

    Specify site_path in feed.blade.atom header, restore to RSS

--- source/_layouts/rss.blade.php
+++ source/_layouts/rss.blade.php
@@ -1,7 +1,7 @@
 <?php echo "<?xml version=\"1.0\"?>\n"; ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
     <title>{{ $page->siteName }}</title>
-    <link href="{{ $page->baseUrl }}" />
+    <link href="{{ rightTrimPath($page->baseUrl) }}/{{ $page->site_path }}" />
     <link type="application/atom+xml" rel="self" href="{{ $page->getUrl() }}" />
     <updated>{{ date(DATE_ATOM) }}</updated>
     <id>{{ $page->getUrl() }}</id>
