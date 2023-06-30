9498e4125bad2fab3c059aeb787250d764788488
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-06-30 17:34:53 -0400

    Update config

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,31 @@
-import { spawn } from 'child_process';
-import { defineConfig } from 'vite';
+import { spawn, exec } from 'child_process';
+import { createLogger, defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2'
 
+const log = createLogger(undefined, { prefix: '[jigsaw]' });
+
 function jigsaw() {
-    spawn('vendor/bin/jigsaw', ['build', 'local', '-q'], { stdio: 'inherit' });
+    const jigsaw = spawn('vendor/bin/jigsaw', ['build', 'local', '-q']);
+    const lines = (data) => String(data).trim().split('\n');
+    jigsaw.stdout.on('data', (data) => lines(data).map((line) => log.info(line, { timestamp: true })))
+    jigsaw.stderr.on('data', (data) => lines(data).map((line) => log.error(line, { timestamp: true })))
+    jigsaw.on('close', (code) => log.info(`Jigsaw build finished with exit code ${code}.`, { timestamp: true }))
 }
 
 export default defineConfig(({ command }) => ({
-    // This might be unnecessary
-    base: command === 'build' ? '/assets/build/' : '/', // https://vitejs.dev/guide/build.html#public-base-path
     publicDir: 'source/assets', // served at / in dev, copied to root of outDir in build
-    // assetsInclude: ['**/*.gltf'], // https://vitejs.dev/config/#assetsinclude
     build: {
-        manifest: true,
         outDir: 'source/assets/build',
+        manifest: true,
+        // Our static assets are already right where we want them
+        copyPublicDir: false,
         rollupOptions: {
             input: 'source/_assets/js/main.js',
         },
     },
     server: {
         watch: {
+            // Don't hot-reload when output or cache files change
             ignored: ['**/build_local/**', '**/cache/**'],
         },
     },
@@ -28,14 +34,12 @@ export default defineConfig(({ command }) => ({
         {
             name: 'jigsaw:serve',
             apply: 'serve',
-            buildStart() {
-                console.log('buildStart');
-                jigsaw();
-            },
+            // Build the Jigsaw site once initially
+            buildStart: jigsaw,
+            // Rebuild when any PHP or Markdown files change
             handleHotUpdate({ file, server }) {
-                console.log('handleHotUpdate');
                 if (file.endsWith('.php') || file.endsWith('.md')) {
-                    console.log(file);
+                    log.info(`Updating (${file.replace(`${__dirname}/`, '')})`, { timestamp: true });
                     jigsaw();
                     server.ws.send({ type: 'full-reload', path: '*' });
                     return [];
@@ -45,10 +49,7 @@ export default defineConfig(({ command }) => ({
         {
             name: 'jigsaw:build',
             apply: 'build',
-            async writeBundle() {
-                console.log('writeBundle')
-                await jigsaw();
-            },
+            writeBundle: jigsaw,
         },
     ],
 }));
